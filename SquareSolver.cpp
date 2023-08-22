bc7af1ead1808b350b8c56ccf47850d96e39650e
OFFlinea <kuznetsov.iv01@gmail.com>
2023-08-22 14:39:41 +0300

    Update SquareSolver.cpp
    доделал некоторые случаи

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -2,25 +2,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void){
-double a, b, c, d;
-printf("Введите коэффиценты квадратного уравнения: ");
-scanf("%lf %lf %lf", &a, &b, &c);
-d = pow(b, 2) - 4 * a * c;
-if (a > 0 || a < 0){
-    if (d > 0){
-        double x1 = (-b + sqrt(d)) / (2 * a);
-        double x2 = (-b - sqrt(d)) / (2 * a);
-        printf("Корни уравнения: %.1lf %.1lf", x1, x2);
+int main(){
+    double a = 0, b = 0, c = 0;
+    printf("Введите коэффиценты квадратного уравнения: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if (a != 0) {
+        const double d = b * b - 4 * a * c;
+        if (d > 0) {
+            const double d_sqrt = sqrt(d);
+            double x1 = (-b + d_sqrt) / (2 * a);
+            double x2 = (-b - d_sqrt) / (2 * a);
+            printf("Корни уравнения: %.1lf %.1lf\n", x1, x2);
+        }
+        else if (d == 0) {
+            double x = -b / (2 * a);
+            printf("Корень уравнения: %.1lf\n", x);
+        }
+        else {
+            printf("У данного уравнения нет корней в действительных величинах.\n");
+        }
     }
-    else if (!(d > 0 || d < 0)){
-        double x = (-b) / (2 * a);
-        printf("Корень уравнения: %.1lf", x);
+    else {
+        if (b != 0) {
+            printf("Корень уравнения: %.1lf\n", -c / b);
+        }
+        else {
+            printf((c != 0) ? "Бесконечное множество корней\n" : "У данного уравнения нет корней в действительных величинах.\n");
+        }
     }
-    else
-    printf("У данного уравнения нет корней в действительных величинах.");
-}
-else
-printf("Это уравнение не квадратное!);
-return 0;
+    return 0;
 }

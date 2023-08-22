aff6b226db43d0b4b0ae24bba2136b6b289c87a8
OFFlinea <kuznetsov.iv01@gmail.com>
2023-08-22 19:58:17 +0300

    Update SquareSolver.cpp
     допилил несколько функций, улучшил ввод

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,75 +1,141 @@
 #include <TXLib.h>
-#include <stdio.h>
+#include <assert.h>
 #include <math.h>
+#include <stdio.h>
 
-const double epsilon = 1e-6;
+const double EPSILON = 1e-6;
+const int DIGITS_AFTER_POINT = 1;
 
 
 bool is_zero(double n);
 
-void square_solver(const double *a, const double *b, const double *c);
+bool is_greater(double n);
+
+void square_solver(const double a, const double b, const double c);
+
+void introduction(void);
+
+void clear_buf(void);
+
+void square_print(const int count_solutions, const double *x1, const double *x2);
 
 
 int main(){
 
-    double a = 0, b = 0, c = 0;
+    double a = 0;
+    double b = 0;
+    double c = 0;
 
-    printf("Эта программа решает квадратное уравнение\n"
-           "Введите коэффиценты квадратного уравнения: ");
+    introduction();
 
-    if(scanf("%lf %lf %lf", &a, &b, &c) != 3) {
+    while(scanf("%lf %lf %lf", &a, &b, &c) != 3) {
+        clear_buf();
 
-        printf("Вы ввели некорректные данные. Вы должны ввести 3 числа через пробелы, попробуйте снова.");
+        printf("Вы ввели некорректные данные."
+        " Вы должны ввести 3 числа через пробелы, попробуйте снова: ");
+
     }
-    else {
+    square_solver(a, b, c);
 
-        square_solver(&a, &b, &c);
-    }
     return 0;
 }
 
 
 bool is_zero(double n) {
 
-    return (fabs(n) < epsilon) ? true : false;
+    assert(isfinite(n));
+
+    return (fabs(n) < EPSILON) ? true : false;
 
 }
 
 
-void square_solver(const double *a, const double *b, const double *c) {
+void square_solver(const double a, const double b, const double c) {
 
-    if (!is_zero(*a)) {
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert(isfinite(c));
 
-        const double d = *b * *b - 4 * *a * *c;
 
-        if (d > 0) {
+    int count_solutions = -1;
+    double x1 = '0';
+    double x2 = '0';
+
+    if (!is_zero(a)) {
+
+        const double d = b * b - 4 * a * c;
+
+        if (is_greater(d)) {
 
             const double d_sqrt = sqrt(d);
-            double x1 = (-*b + d_sqrt) / (2 * *a);
-            double x2 = (-*b - d_sqrt) / (2 * *a);
-            printf("Корни уравнения: %.1lf %.1lf\n", x1, x2);
+            x1 = (-b + d_sqrt) / (2 * a);
+            x2 = (-b - d_sqrt) / (2 * a);
+            count_solutions = 2;
         }
 
         else if (is_zero(d)) {
 
-            double x = -*b / (2 * *a);
-            printf("Корень уравнения: %.1lf\n", x);
+            x1 = -b / (2 * a);
+            count_solutions = 1;
         }
 
         else {
-            printf("У данного уравнения нет корней в действительных величинах.\n");
+
+            count_solutions = 0;
         }
     }
 
     else {
 
-        if (!is_zero(*b)) {
+        if (!is_zero(b)) {
 
-            printf("Корень уравнения: %.1lf\n", -*c / *b);
+            x1 = -c / b;
+            count_solutions = 1;
         }
 
         else {
-            printf((is_zero(*c)) ? "Бесконечное множество корней\n" : "У данного уравнения нет корней в действительных величинах.\n");
+
+            count_solutions = (is_zero(c)) ? 3 : 0;
         }
+    }
+    square_print(count_solutions, &x1, &x2);
+}
+
+
+void introduction(void){
+    printf("Эта программа решает квадратное уравнение\n"
+           "Введите коэффиценты квадратного уравнения: ");
+}
+
+
+void clear_buf(void) {
+    int c = getchar();
+    while((c = getchar()) != '\n');
+}
+
+
+bool is_greater(double n){
+    return (n > EPSILON) ? true : false;
+}
+
+
+void square_print(const int count_solutions, const double *x1, const double *x2){
+
+    assert(isfinite(*x1));
+    assert(isfinite(*x2));
+
+    switch(count_solutions) {
+        case 0:
+            printf("У данного уравнения нет корней в действительных величинах.\n");
+            break;
+        case 1:
+            printf("Корень уравнения: %.*lf\n", DIGITS_AFTER_POINT, *x1);
+            break;
+        case 2:
+            printf("Корни уравнения: %.*lf %.*lf\n", DIGITS_AFTER_POINT, *x1, DIGITS_AFTER_POINT, *x2);
+            break;
+        default:
+            printf("Бесконечное множество решений.");
+            break;
     }
 }
